380995c273b9f920557f0304ba63414e0b9f53d7
Juan Francisco Kurucz <juanfkurucz@gmail.com>
2022-03-31 19:45:58 -0300

    Add wed30 excerise from class 30 march

---
+++ wed30/wed30/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wed30
+//
+//  Created by Juan Francisco Kurucz on 30/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
