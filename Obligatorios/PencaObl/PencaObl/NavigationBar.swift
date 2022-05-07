b630b9301e5f8dceab69165c882f0498723fa55e
Juan Francisco Kurucz <juanfkurucz@gmail.com>
2022-05-07 20:09:30 -0300

    Add logo in navigation bar item

---
+++ Obligatorios/PencaObl/PencaObl/NavigationBar.swift
@@ -0,0 +1,15 @@
+//
+//  NavigationBar.swift
+//  PencaObl
+//
+//  Created by Juan Francisco Kurucz on 7/5/22.
+//
+
+import UIKit
+
+class NavigationBar: UINavigationBar {
+
+    override func sizeThatFits(_ size: CGSize) -> CGSize {
+        return CGSize(width: UIScreen.main.bounds.width, height: 80)
+    }
+}
---
+++ Obligatorios/PencaObl/PencaObl/Utils/Visual.swift
@@ -0,0 +1,25 @@
+//
+//  Visual.swift
+//  PencaObl
+//
+//  Created by Juan Francisco Kurucz on 7/5/22.
+//
+
+import Foundation
+import UIKit
+
+
+class Visual {
+    static func addNavBarImage(element: UIViewController) {
+        let titleView = UIView(frame: CGRect(x: element.navigationController!.navigationBar.frame.minX, y: element.navigationController!.navigationBar.frame.minY, width: element.navigationController!.navigationBar.frame.width, height:element.navigationController!.navigationBar.frame.height))
+        
+        let imageView = UIImageView(image:  UIImage(named: "logo"))
+        let imageWidth = CGFloat(170)
+        let imageHeight = CGFloat(30)
+        imageView.frame = CGRect(x: (titleView.frame.width)/2-imageWidth/2, y: (titleView.frame.height)/2, width: imageWidth, height: imageHeight)
+        imageView.contentMode = .scaleAspectFit
+        titleView.addSubview(imageView)
+        titleView.backgroundColor = .clear
+        element.navigationItem.titleView = titleView
+    }
+}
